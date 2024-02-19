4a112e6c49e7c6f62a09b63d12c02c9ae54fd685
Francisco Enriquez <Francisco.R.Enriquez@gmail.com>
2024-02-19 01:44:52 +0000

    All tests pass.

--- prep/sorting/medium/minimum_swaps_2/minimum_swaps_2.py
+++ prep/sorting/medium/minimum_swaps_2/minimum_swaps_2.py
@@ -7,10 +7,12 @@ array in ascending order.
 
 Can use numpy.argmin to find min but nupmy is not allowed for this 
 problem on Hackerrank.
+
+Using enumerate to create a dict {number: pos_in_array} allows fast
+lookup and passes all tests.
 """
 
 import math
-import numpy as np
 
 
 def find_min(arr, lt, rt, target):
@@ -28,15 +30,16 @@ def find_min(arr, lt, rt, target):
 # Complete the minimumSwaps function below.
 def minimumSwaps(arr):
     min_swaps = 0
+    lookup = {key: val for val, key in enumerate(arr, start=1)}
     for i in range(len(arr)):
         # check if arr[i] is in the correct order
         if arr[i] == (i + 1):
             continue
-        # find the number that goes in arr[i]
-        min_i = find_min(arr, i, len(arr) - 1, i + 1)
-        if arr[min_i] == (i + 1):
-            tmp = arr[i]
-            arr[i] = arr[min_i]
-            arr[min_i] = tmp
-            min_swaps += 1
+        # find the number that goes in arr[i] and swap
+        min_i = lookup[i + 1]
+        tmp = arr[i]
+        lookup[tmp] = min_i
+        arr[i] = arr[min_i - 1]
+        arr[min_i - 1] = tmp
+        min_swaps += 1
     return min_swaps
--- prep/sorting/medium/minimum_swaps_2/test_minimum_swaps_2.py
+++ prep/sorting/medium/minimum_swaps_2/test_minimum_swaps_2.py
@@ -1,15 +1,13 @@
 import pytest
 from prep.sorting.medium.minimum_swaps_2.minimum_swaps_2 import minimumSwaps
 
-""" 9/14 tests pass on hackerrank with solution but I get error:
-Time limit exceeded
-Your code did not execute within the time limits. Please optimize
-your code. For more information on execution time limits, refer 
-to the environment page
+""" 14/14 tests pass on hackerrank with solution.
 """
 
 
-@pytest.mark.parametrize(("arr", "expected"), [([7, 1, 3, 2, 4, 5, 6], 5)])
+@pytest.mark.parametrize(
+    ("arr", "expected"), [([7, 1, 3, 2, 4, 5, 6], 5), ([4, 3, 1, 2], 3)]
+)
 def test_minimum_swaps(arr, expected):
     # WHEN
     actual = minimumSwaps(arr)
